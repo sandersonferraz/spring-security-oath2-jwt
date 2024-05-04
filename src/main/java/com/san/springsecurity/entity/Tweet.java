d767390aca42403be3d9fb959936b61644e5a4e1
sandersonferraz <sandersonferraz@gmail.com>
2024-05-04 00:08:47 -0300

    Create users entities, roles, and tweets

---
+++ src/main/java/com/san/springsecurity/entity/Tweet.java
@@ -0,0 +1,26 @@
+package com.san.springsecurity.entity;
+
+import com.san.springsecurity.entity.auth.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_tweets")
+public class Tweet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "tweet_id")
+    private Long tweetId;
+    private User user;
+    private String content;
+    @CreationTimestamp
+    private Instant creationTimestamp;
+
+}
---
+++ src/main/java/com/san/springsecurity/entity/auth/User.java
@@ -0,0 +1,32 @@
+package com.san.springsecurity.entity.auth;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID userId;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "tb_users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+}
